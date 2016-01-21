9af419e088e575866ea39cda887d5cb5891d4162
Florian Hengartner <fhengartner@users.noreply.github.com>
2016-01-21 21:05:02 +0100

    add entity Rate and RatesRepository

---
+++ src/main/java/biz/hengartner/euroexchange/domain/Rate.java
@@ -0,0 +1,35 @@
+package biz.hengartner.euroexchange.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Rate {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@NotNull
+	private String currency;
+
+	@NotNull
+	private BigDecimal rate;
+
+    // TODO: use LocalDate
+	@NotNull
+	private String date;
+
+}
+
---
+++ src/main/java/biz/hengartner/euroexchange/domain/RatesRepository.java
@@ -0,0 +1,13 @@
+package biz.hengartner.euroexchange.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+
+@Transactional
+public interface RatesRepository extends JpaRepository<Rate, Long> {
+
+    Rate findByCurrencyAndDate(@Param("currency") String currency, @Param("date") String date);
+
+}
