a9fbd3677e36d8646826b3f57d427c36639927f8
Florian Hengartner <fhengartner@users.noreply.github.com>
2016-01-21 21:24:55 +0100

    implement path /api/rates/{currency}/{date}

--- src/main/java/biz/hengartner/euroexchange/controller/RatesController.java
+++ src/main/java/biz/hengartner/euroexchange/controller/RatesController.java
@@ -1,20 +1,39 @@
 package biz.hengartner.euroexchange.controller;
 
+import biz.hengartner.euroexchange.domain.Rate;
+import biz.hengartner.euroexchange.domain.RatesRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/api/rates")
 @Controller
 public class RatesController {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    private RatesRepository ratesRepository;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{currency}/{date}")
     @ResponseBody
-    @RequestMapping("/api/rates")
-    public String index() {
-        log.debug("Logging works!");
-        return "Hello World!";
+    public ResponseEntity<Rate> rateByCurrencyAndDate(
+            @PathVariable("currency") String currency,
+            @PathVariable("date") String dateString) {
+
+        log.info("rates by currency: {}, date: {}", currency, dateString);
+        Rate rate = ratesRepository.findByCurrencyAndDate(currency, dateString);
+
+        log.debug("Found rate: {}", rate);
+
+        return new ResponseEntity<>(rate, HttpStatus.OK);
     }
+
 }
--- src/test/java/biz/hengartner/euroexchange/controller/RatesControllerTest.java
+++ src/test/java/biz/hengartner/euroexchange/controller/RatesControllerTest.java
@@ -1,10 +1,13 @@
 package biz.hengartner.euroexchange.controller;
 
 import biz.hengartner.euroexchange.Application;
+import biz.hengartner.euroexchange.domain.Rate;
+import biz.hengartner.euroexchange.domain.RatesRepository;
 import com.jayway.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -12,13 +15,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.math.BigDecimal;
+
 import static com.jayway.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 @IntegrationTest("server.port:0")
 public class RatesControllerTest {
+
+    @Autowired
+    private RatesRepository ratesRepository;
 
     @Value("${local.server.port}")
     int port;
@@ -29,10 +38,15 @@ public class RatesControllerTest {
     }
 
     @Test
-    public void canAccess() {
+    public void retrieveByCurrencyAndDate() {
+        // given
+        Rate rate = new Rate(null, "USD", new BigDecimal("1.21"), "2016-01-01");
+        ratesRepository.saveAndFlush(rate);
+
         when().
-                get("/api/rates").
+                get("/api/rates/" + rate.getCurrency() + "/" + rate.getDate()).
         then().
-                statusCode(HttpStatus.OK.value());
+                statusCode(HttpStatus.OK.value()).
+                body(containsString(rate.getRate().toString()));
     }
 }
