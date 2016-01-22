6926e7eeb97f39ca87de18f03c424ae91c5b4a91
Florian Hengartner <fhengartner@users.noreply.github.com>
2016-01-22 13:10:43 +0100

    remove main method

--- src/main/java/biz/hengartner/euroexchange/ecb/EurofxXmlParser.java
+++ src/main/java/biz/hengartner/euroexchange/ecb/EurofxXmlParser.java
@@ -9,7 +9,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * Parses the xml format as used by:
@@ -33,16 +32,6 @@ public class EurofxXmlParser {
     public List<CubeWithTime> parseXml(URL url) throws IOException {
         XmlMapper xmlMapper = createXmlMapper();
         return envelopeToCubeWithTimeList(xmlMapper.readValue(url, Envelope.class));
-    }
-
-    public static void main(String[] args) throws IOException {
-        String url = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
-        String url2 = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml\n";
-
-        EurofxXmlParser parser = new EurofxXmlParser();
-        String out = new Scanner(new URL(url2).openStream(), "UTF-8").useDelimiter("\\A").next();
-
-        System.out.println(parser.parseXml(out).get(0).getCubes().size());
     }
 
     /**
