c3d85c3db9f8947ca8878e3267cde3afe78d11de
Florian Hengartner <fhengartner@users.noreply.github.com>
2016-01-22 02:43:51 +0100

    add path /api/rates/status for querying StatusService

--- src/main/java/biz/hengartner/euroexchange/api/controller/RatesController.java
+++ src/main/java/biz/hengartner/euroexchange/api/controller/RatesController.java
@@ -2,6 +2,7 @@ package biz.hengartner.euroexchange.api.controller;
 
 import biz.hengartner.euroexchange.api.domain.Rate;
 import biz.hengartner.euroexchange.api.domain.RatesRepository;
+import biz.hengartner.euroexchange.api.service.StatusService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,16 @@ public class RatesController {
     @Autowired
     private RatesRepository ratesRepository;
 
+    @Autowired
+    private StatusService statusService;
+
+
+    @RequestMapping(method = RequestMethod.GET, path = "/status")
+    @ResponseBody
+    public ResponseEntity<String> status() {
+        return new ResponseEntity<>("isReady: " + Boolean.toString(statusService.isReady()), HttpStatus.OK);
+    }
+
     @RequestMapping(method = RequestMethod.GET, path = "/{currency}/{date}")
     @ResponseBody
     public ResponseEntity<Rate> rateByCurrencyAndDate(
@@ -33,9 +44,19 @@ public class RatesController {
 
         log.info("rates by currency: {}, date: {}", currency, dateString);
 
+        if (!statusService.isReady()) {
+            log.warn("service is not ready, will not query database!");
+            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+        }
+
         LocalDate date = LocalDate.parse(dateString, DateTimeFormatter.ISO_DATE);
 
         Rate rate = ratesRepository.findByCurrencyAndDate(currency, date);
+
+        if (rate == null) {
+            log.warn("unable to find rate for currency: {}, date: {}", currency, dateString);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
         log.debug("Found rate: {}", rate);
 
